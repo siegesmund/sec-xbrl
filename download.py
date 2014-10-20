6fc8e312eda1e2f78882744f2736cbdae5144b94
Peter Siegesmund <peter.siegesmund@gmail.com>
2014-10-20 09:46:42 -0400

    bugfix

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 from loadSECfilings import SECdownload
 
 def download():
-    for year in range(2012, 2015):
-        for month in range(1, 13):
-            SECdownload(2014, 10)
+	for year in range(2012, 2015):
+		for month in range(1, 13):
+			SECdownload(year, month)
